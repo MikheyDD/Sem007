c9b9b9e7662bcbc950c08004e69f5a0e43188440
Mikheyev_Denis <mikheydd@mail.ru>
2022-12-02 23:26:52 +0300

    Sem007

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[,] num = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 0, 5, 3 } };
+
+// int[,] a = new int[2,3];
+// int[,,] num2 = new int[2, 3, 4];
+
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
